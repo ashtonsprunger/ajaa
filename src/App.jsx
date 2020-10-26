b940677a72eaabcac5991febe5ad6bc9faa8143c
Addy Monger <AddyMonger@Addys-MacBook-Air.local>
2020-10-26 19:11:50 -0400

    addy 10/26

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import FoodComponent from './Components/FoodComponent.jsx';
 
 function App() {
   return (
@@ -15,9 +16,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
       </header>
+      <FoodComponent />
     </div>
   );
 }
---
+++ src/Components/FoodComponent.jsx
@@ -0,0 +1,44 @@
+
+import React from 'react';
+
+
+const Food = (props) => {
+
+     let apiKey = "Wy1wADvYUr6cNdNhe7aFJzcWT9Zk9J5hyEbIPuR4";
+     let latitude = props.latitude;
+     let longitude = props.longitude;
+     let url = `https://developers.zomato.com/api/v2.1/search?lat=${latitude}&lon=${longitude}&appikey=${apiKey}`;
+
+
+     return(
+          <div>
+               
+          </div>
+          );
+}
+
+getLocation();
+ 
+ navigator.geolocation.getCurrentPosition (
+        (position) => {
+            console.log("Latitude is :", position .coords.latitude);
+            console.log("Longitude is :", position.coords.longitude);
+            this.setState({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            })
+            this.Food();
+        },
+        function(error) {
+            console.error("Error code =" + error.code + "-" + error.message);
+        },
+        {
+            enableHighAccuracy: false,
+            timeout: 10000
+        }
+
+    );
+
+
+
+export default Food;
