118ec4804d4207e589b30d6fc9a19eb8372af4df
Addy Monger <AddyMonger@Addys-MacBook-Air.local>
2020-10-26 20:54:36 -0400

    Addy Food 10/26

--- src/Components/FoodComponent.jsx
+++ src/Components/FoodComponent.jsx
@@ -4,11 +4,25 @@ import React from 'react';
 const Food = (props) => {
 
      let apiKey = "Wy1wADvYUr6cNdNhe7aFJzcWT9Zk9J5hyEbIPuR4";
-     let latitude = props.latitude;
-     let longitude = props.longitude;
+     let latitude = props.lat;
+     let longitude = props.lon;
      let url = `https://developers.zomato.com/api/v2.1/search?lat=${latitude}&lon=${longitude}&appikey=${apiKey}`;
 
 };
+
+fetch("https://developers.zomato.com/api/v2.1/search?entity_id=280&entity_type=city&category=dinner&sort=cost", { headers: { "user-key": apiKey } }
+)
+  .then(response => {
+    return response.json();
+  })
+  .then(json => {
+    json.restaurants.forEach(restaurant => {
+      console.log(restaurant.restaurant.name);
+      console.log(restaurant.restaurant.cuisines)
+      console.log(restaurant.restaurant.location.address)
+      console.log(restaurant.restaurant.location.city);
+    });
+  });
 
 getLocation();
  
@@ -32,6 +46,21 @@ getLocation();
 
     );
 
+    return(
+        <div>
+             <span>
+                  <Row>
+                       <span>Name: {name}</span>
+                  </Row>
+                  <Row>
+                       <span>Type of Food: {cuisines}</span>
+                  </Row>
+                  <Row>
+                       <span>Address: {address}</span>
+                  </Row>
+             </span>
+        </div>
+        );
 
 
 export default Food;
