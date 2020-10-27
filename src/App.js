e05facbff5252e20f89bca346e3370f4d1b6cffe
ashtonsprunger <sprungerashton@gmail.com>
2020-10-27 09:24:30 -0400

    a-working-app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import React, { useState, useEffect } from "react";
+import { Row, Col } from "reactstrap";
 import NasaComponent from "./Components/NasaComponent";
+import FoodSecondComponent from "./Components/FoodSecondComponent";
+// import WeatherComponent from "./Components/WeatherComponent";
 
 function App() {
-
-  const [lat, setLatitude] = useState('');
-  const [lon, setLongitude] = useState('');
-  const [temperatureType, setTemperature] = useState('');
+  const [lat, setLatitude] = useState("");
+  const [lon, setLongitude] = useState("");
+  const [temperatureType, setTemperature] = useState("");
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -29,17 +31,26 @@ function App() {
 
   return (
     <div className="App">
-      <Row><h1>Welcome to ajaa</h1></Row>
       <Row>
-        <Col xs="6"><h2>Lat: {lat}</h2></Col>
-        <Col xs="6"><h2>Lon: {lon}</h2></Col>
-       </Row>
+        <h1>Welcome to ajaa</h1>
+      </Row>
       <Row>
-        <Col xs="4"><NasaComponent lat={lat} lon={lon}/></Col>
-        <Col xs="4"><WeatherComponent lat={lat} lon={lon}/></Col>
-        <Col xs="4"><FoodComponent lat={lat} lon={lon}/></Col> 
-      </Row>   
-
+        <Col xs="6">
+          <h2>Lat: {lat}</h2>
+        </Col>
+        <Col xs="6">
+          <h2>Lon: {lon}</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs="4">
+          <NasaComponent lat={lat} lon={lon} />
+        </Col>
+        <Col>{/* <WeatherComponent /> */}</Col>
+        <Col xs="4">{/* <WeatherComponent lat={lat} lon={lon} /> */}</Col>
+        {/* <Col xs="4"><FoodComponent lat={lat} lon={lon}/></Col>  */}
+        <FoodSecondComponent />
+      </Row>
 
       <div>
         <label className="switch">
