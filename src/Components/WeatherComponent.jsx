cbbf8f6494981f5c1e659d2761f0d4e13bcd26ac
Jonathan Ingersoll <jonathan.ingersoll@gmail.com>
2020-10-27 11:59:53 -0400

    finalizing weather component and toggle button

--- src/Components/WeatherComponent.jsx
+++ src/Components/WeatherComponent.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import {
+     Button,
      Col,
      Row
 } from 'reactstrap';
@@ -12,59 +13,74 @@ import {
 // api.openweathermap.org/data/2.5/weather?lat=39.770639&lon=-86.285116&appid=70d415361464c75295b5f22c9849e8c3
 
 const Weather = (props) => {
-
-     const key = props.weather_key;
-     let lat = props.lat;
-     let lon = props.lon;
-     let url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`;
-
+     console.log(props.lat, props.lon);
+     
+     
      const [ description, setDescription] = useState('');
      const [ temperature, setTemperature] = useState('');
-     const [ feels, setFeels            ] = useState('');
      const [ pressure, setPressure      ] = useState('');
      const [ humidity, setHumidity      ] = useState('');
      const [ windSpeed, setWindSpeed    ] = useState('');
-
+     const [ units, setUnits ] = useState('units=imperial');
+     const [ butText, setButText ] = useState('Switch to Metric');
+     
+     const key = '70d415361464c75295b5f22c9849e8c3';
+     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${props.lat}&lon=${props.lon}&appid=${key}&${units}`;
+     
      const getWeather = () => {
-          fetch(url).then((res) => {
-               setDescription(res.weather.description);
-               setTemperature((((res.main.temp - 273.15)*9) / 5) + 32);
-               setFeels(res.main.feels_like);
-               setPressure(res.main.pressure);
-               setHumidity(res.main.humidity);
-               setWindSpeed(res.wind.windSpeed);
+          fetch(url)
+          .then(res => res.json())
+          .then((data) => {
+               setDescription(data.weather[0].description);
+               setTemperature(data.main.temp);
+               setPressure(data.main.pressure);
+               setHumidity(data.main.humidity);
+               setWindSpeed(data.wind.speed);
           })
      }
 
-     const fToC = (temp) => {
-          let c = temp - 32 * 5/9
-     }
+     useEffect(getWeather, [units]);
 
-     useEffect(getWeather, []);
+     const changeUnits = (e) => {
+          if(units == 'units=metric'){
+               setUnits('units=imperial');
+               setButText('Switch to Metric');
+          } else{
+               setUnits('units=metric');
+               setButText('Switch to Imperial');
+          }
+     }
 
      return(
           <>
-               {/* switch goes here */}
-               <Row>
-                    <span>Current weather: {description}</span>
+               <Row id="head-row">
+                    <Col>
+                         Set Imperial (default) or Metric:
+                    </Col>
+                    <Col>
+                    <div>
+                         <Button onClick={(e) => changeUnits(e)}>{butText}</Button>
+                         
+                         </div>
+                    </Col>
                </Row>
                <Row>
-                    <span>Temperature: {temperature}</span>
+                    <span><strong>Current weather: </strong>{description}</span>
                </Row>
                <Row>
-                    <span>Feels like: {feels}</span>
+                    <span><strong>Temperature: </strong>{temperature}</span>
                </Row>
                <Row>
-                    <span>Air pressure: {pressure}</span>
+                    <span><strong>Air pressure: </strong>{pressure}</span>
                </Row>
                <Row>
-                    <span>Humidity: {humidity}</span>
+                    <span><strong>Humidity: </strong>{humidity}%</span>
                </Row>
                <Row>
-                    <span>Wind speed: {windSpeed}</span>
+                    <span><strong>Wind speed: </strong>{windSpeed}</span>
                </Row>
-               </>
-          );
+          </>
+     );
 }
 
 export default Weather;
